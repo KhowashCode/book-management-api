44048a6bf402bbbaa9bf5d3d12be34a6fdc2a027
Mohammad Bulghotul Khowash <git.khowash@gmail.com>
2026-08-15 12:11:01 +0700

    chore: menambahkan koneksi ke database dan route

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"book-management-api/database"
+	"book-management-api/routes"
+	"os"
+)
 
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8080"
+	}
+
+	database.ConnectToDatabase()
+	routes.Server().Run(":" + port)
 }
